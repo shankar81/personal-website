0eeda178be64471f8bacd1147b54b7dbb956c71d
shankar81 <shankarsawant81@gmail.com>
2021-03-04 20:50:00 +0530

    :arrow_up: Navigation Added
    Installed and setup react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import Header from "./components/Header/Header";
 import Theme from "./components/Theme/Theme";
 import Contact from "./pages/Contact/Contact";
 import { GlobalContext } from "./utils/GlobalContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./pages/About/About";
+import LandingPage from "./pages/Landing/LandingPage";
 
 export default function App() {
   const [theme, setTheme] = React.useState("light"); // light | dark
@@ -22,16 +25,18 @@ export default function App() {
   return (
     <GlobalContext.Provider value={{ isBackdropOpen, onToggleBackdrop }}>
       <Theme theme={theme}>
-        <div className="scroll-mode">
-          <Header onChangeTheme={toggleTheme} />
-          {/**
-           * @TODO Add routing here
-           * */}
-          {/* <LandingPage /> */}
-          <Contact />
-          <Footer />
-          {isBackdropOpen && <Backdrop />}
-        </div>
+        <Router>
+          <div className="scroll-mode">
+            <Header onChangeTheme={toggleTheme} />
+            <Switch>
+              <Route component={LandingPage} exact path="/" />
+              <Route component={About} exact path="/about" />
+              <Route component={Contact} exact path="/contact" />
+            </Switch>
+            <Footer />
+            {isBackdropOpen && <Backdrop />}
+          </div>
+        </Router>
       </Theme>
     </GlobalContext.Provider>
   );
